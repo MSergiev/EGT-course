3934a885e8e16f445f6d6bd9f2b2cf06809b773b
Mitko Nikolov <dnnmitko@gmail.com>
2017-05-15 12:16:04 +0300

    Added some basics for the Triangle class
    Added default, parameter and copy constructors, destructor, get and set
    methods.

--- Triangle/Triangle.cpp
+++ Triangle/Triangle.cpp
@@ -1,12 +1,88 @@
 #include "Triangle.h"
 
-Triangle::Triangle()
+Triangle::Triangle(int a, int b, int c, int h)
 {
-	// TODO Auto-generated constructor stub
+	_init(a, b, c, h);
+}
+
+Triangle::Triangle(const Triangle& other)
+{
+	_copy(other);
 }
 
 Triangle::~Triangle()
 {
-	// TODO Auto-generated destructor stub
+	_delete();
 }
+
+void Triangle::_init(int a, int b, int c, int h)
+{
+	setA(a);
+	setB(b);
+	setC(c);
+	setH(h);
+}
+
+void Triangle::_copy(const Triangle& other)
+{
+	setA(other.getA());
+	setB(other.getB());
+	setC(other.getC());
+	setH(other.getH());
+}
+
+void Triangle::_delete()
+{
+	//No need to do anything
+}
+
+int Triangle::getA() const
+{
+	return a;
+}
+
+void Triangle::setA(int a)
+{
+	this->a = a;
+}
+
+int Triangle::getB() const
+{
+	return b;
+}
+
+void Triangle::setB(int b)
+{
+	this->b = b;
+}
+
+int Triangle::getC() const
+{
+	return c;
+}
+
+void Triangle::setC(int c)
+{
+	this->c = c;
+}
+
+int Triangle::getH() const
+{
+	return h;
+}
+
+void Triangle::setH(int h)
+{
+	this->h = h;
+}
+
+
+
+
+
+
+
+
+
+
 
--- Triangle/Triangle.h
+++ Triangle/Triangle.h
@@ -1,11 +1,37 @@
 #ifndef TRIANGLE_H_
 #define TRIANGLE_H_
 
+#include <iostream>
+using std::istream;
+using std::ostream;
+
 class Triangle
 {
 public:
-	Triangle();
+	Triangle(int = 0, int = 0, int = 0, int = 0);
+	Triangle(const Triangle&);
 	~Triangle();
+public:
+	Triangle& operator=(const Triangle&);
+	friend istream& operator>>(istream&, Triangle&);
+	friend ostream& operator<<(ostream&, Triangle&);
+	bool operator==(const Triangle&);
+	bool operator!=(const Triangle&);
+public:
+	int getA() const;
+	void setA(int a);
+	int getB() const;
+	void setB(int b);
+	int getC() const;
+	void setC(int c);
+	int getH() const;
+	void setH(int h);
+private:
+	void _init(int, int, int, int);
+	void _copy(const Triangle&);
+	void _delete();
+private:
+	int a, b, c, h;
 };
 
 #endif /* TRIANGLE_H_ */
