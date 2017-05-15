ed9b2c0c89e636e30fd8b6ea3d196a3dd147a5a2
Mitko Nikolov <dnnmitko@gmail.com>
2017-05-15 11:51:14 +0300

    Added main function and basis for Triangle class

---
+++ Triangle/Triangle.cpp
@@ -0,0 +1,12 @@
+#include "Triangle.h"
+
+Triangle::Triangle()
+{
+	// TODO Auto-generated constructor stub
+}
+
+Triangle::~Triangle()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Triangle/Triangle.h
@@ -0,0 +1,11 @@
+#ifndef TRIANGLE_H_
+#define TRIANGLE_H_
+
+class Triangle
+{
+public:
+	Triangle();
+	~Triangle();
+};
+
+#endif /* TRIANGLE_H_ */
