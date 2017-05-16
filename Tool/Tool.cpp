0b42277af943125bb2b267aa78de867e977f4456
Mitko Nikolov <dnnmitko@gmail.com>
2017-05-16 13:10:20 +0300

    Added Tool project

---
+++ Tool/Tool.cpp
@@ -0,0 +1,92 @@
+#include "Tool.h"
+
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+Tool::Tool(int recordNum, string name, int count, double cost)
+{
+	setRecordNum(recordNum);
+	setName(name);
+	setCount(count);
+	setCost(cost);
+}
+
+Tool::~Tool()
+{
+
+}
+
+double Tool::getCost() const
+{
+	return cost;
+}
+
+void Tool::setCost(double cost)
+{
+	this->cost = cost;
+}
+
+int Tool::getCount() const
+{
+	return count;
+}
+
+void Tool::setCount(int count)
+{
+	this->count = count;
+}
+
+string Tool::getName() const
+{
+	return name;
+}
+
+void Tool::setName(const string& name)
+{
+	const char *nameValue = name.data();
+	int length = name.size();
+	if(length > 9)
+		length = 9;
+	strncpy(this->name, nameValue, length);
+	this->name[length] = '\0';
+}
+
+int Tool::getRecordNum() const
+{
+	return recordNum;
+}
+
+void Tool::setRecordNum(int recordNum)
+{
+	this->recordNum = recordNum;
+}
+
+void Tool::print()
+{
+	cout << left << setw(5) << getRecordNum()
+				  << setw(11) << getName()
+				  << setw(8) << getCount()
+				  << setw(8) << getCost() << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tool/Tool.h
@@ -0,0 +1,30 @@
+#ifndef TOOL_H_
+#define TOOL_H_
+
+#include <string>
+using std::string;
+
+class Tool
+{
+public:
+	Tool(int = 0, string = "NaN", int = 0, double = 0);
+	~Tool();
+public:
+	double getCost() const;
+	void setCost(double cost);
+	int getCount() const;
+	void setCount(int count);
+	string getName() const;
+	void setName(const string& name);
+	int getRecordNum() const;
+	void setRecordNum(int recordNum);
+public:
+	void print();
+private:
+	int recordNum;
+	char name[10];
+	int count;
+	double cost;
+};
+
+#endif /* TOOL_H_ */
